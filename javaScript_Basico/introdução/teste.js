ca60cda049a691b5f2aeba45bd332fe36a0c5c78
Leandro Paulino <Leandropaulino890@gmail.com>
2023-05-22 20:17:40 -0300

    .

---
+++ javaScript_Basico/introdução/teste.js
@@ -0,0 +1,15 @@
+//console.log("Minha primeira aula javaScript. Óla mundo!");//
+//let x = 10;//
+//onsole.log(x);//
+let preco 
+preco = 0.18
+
+console.log("Preço do pão: R$ 0.18 \nPanificadora Pão nosso de cada dia\nTabela de preço");
+
+
+
+for (let i=1; i <= 50; i++) {
+    valor = preco*i
+    console.log(i+" - " + "R$" + valor.toFixed(2))
+}
+
