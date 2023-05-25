56b5f88ddb671afaa6b9d81492778c24057bd801
Leandro Paulino <Leandropaulino890@gmail.com>
2023-05-24 21:14:56 -0300

    estudo

--- javaScript_Basico/introdução/teste.js
+++ javaScript_Basico/introdução/teste.js
@@ -1,15 +1,40 @@
 //console.log("Minha primeira aula javaScript. Óla mundo!");//
 //let x = 10;//
 //onsole.log(x);//
-let preco 
-preco = 0.18
+/*let body = document.body
+let h2 = document.createElement('h2')
+h2.textContent = "Manipulando o DOM"
+body.append(h2)*/
 
-console.log("Preço do pão: R$ 0.18 \nPanificadora Pão nosso de cada dia\nTabela de preço");
-
-
-
-for (let i=1; i <= 50; i++) {
-    valor = preco*i
-    console.log(i+" - " + "R$" + valor.toFixed(2))
+let pessoas=["Ana","Maria","João","Luiz"]
+let lista = document.querySelector('#lista');
+for (let i=0; i<pessoas.length; i++){
+    let li=document.createElement('li')
+    li.textContent=pessoas[i]
+    lista.append(li)
 }
+
+let li2=document.createElement('li')
+    li2.textContent="Maciel"
+    lista.append(li2)
+
+// Outra forma
+
+document.getElementsByTagName('li')[2].textContent="Lidi"
+
+function clicou(){
+    let lista = document.querySelector('#lista');
+    lista.innerHTML += "<li>Geane</li>"
+    // let lista = document.querySelector('#lista').innerHTML+= "<li>Geane</li>"
+    //  let lista = document.querySelector('ul').innerHTML+= "<li>Geane</li>"
+}
+
+/*botao = document.querySelector('#botao')
+botao.addEvenListener("click", clicou);*/
+
+// Outra forma
+// botao = document.querySelector('#botao')
+// botao.addEvenListener("click", function() {clicou});
+
+
 
