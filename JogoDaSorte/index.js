56acc4f33065fd287ee33c51df341e08f7452323
Leandro Paulino <Leandropaulino890@gmail.com>
2023-09-04 19:02:30 -0300

    analisado

--- JogoDaSorte/index.js
+++ JogoDaSorte/index.js
@@ -1,72 +1,67 @@
 const numeroDoApostador = document.getElementById('entrada');
 const palpite = numeroDoApostador;
-
-
-
 //Lista para guardar os valores digitados
 const listaDePalpite = [];
 // Variavel para receber números de tentaivas
 const tentativas = 0;
 
+function enviar() {
+        const num = 62;
+        if (palpite.value > 0) {
 
-function enviar(){
-        const num =  62;
-        if(palpite.value>0){ 
-                
-                if(palpite.value<num){ 
+                if (palpite.value < num) {
                         document.getElementById('maior').innerHTML = '';
                         document.getElementById('acertou').innerHTML = '';
                         const linha1 = "Palpite do Jogador: ";
                         const linha2 = "Número Sorteado: ";
                         const texto = linha1 + palpite.value + "<br>";
                         const digitado = palpite.value;
-                        
-                        
+
                         // adiciona o palpite a lista.
                         listaDePalpite.push(palpite.value);
-        
+
                         // Usei join() para criar uma string com os elementos da lista
                         const listaDePalpitesString = listaDePalpite.join(", ");
-        
+
                         // Limpar o campo de entrada
                         document.getElementById('entrada').value = '';
-                        
+
                         const errouElement = document.getElementById('errou');
                         errouElement.innerHTML = texto + "Números Digitados: " + listaDePalpitesString + "<br><br>" + "Você errou !!!";
-                        
+
                         // Adicione a classe CSS para tornar o texto vermelho
                         errouElement.classList.add('texto-vermelho');
-        
+
                         const menorElement = document.getElementById('menor');
-                        menorElement.innerHTML = "Digite um palpite MAIOR que "+digitado+ " e MENOR que 63.";
-                        
+                        menorElement.innerHTML = "Digite um palpite MAIOR que " + digitado + " e MENOR que 63.";
+
                         // Adicione a classe CSS para tornar o texto vermelho
                         menorElement.classList.add('texto-amarelo');
 
-                        
+                        // Adicionei um som ao errar palpite
                         var audio = new Audio('faustao-errou.mp3');
-                        audio.addEventListener('canplaythrough', function() {
-                        audio.play();
+                        audio.addEventListener('canplaythrough', function () {
+                                audio.play();
                         });
                 }
-                
-                if(palpite.value==num){
+
+                if (palpite.value == num) {
                         document.getElementById('menor').innerHTML = '';
                         document.getElementById('maior').innerHTML = '';
                         document.getElementById('errou').innerHTML = '';
                         const linha1 = "Palpite do Jogador: "
                         const linha2 = "Número Sorteado: "
                         const texto = linha1 + palpite.value + "<br>" + linha2 + num + "<br>";
-                        
+
                         // adiciona o palpite a lista.
                         listaDePalpite.push(palpite.value);
-        
+
                         // Usei join() para criar uma string com os elementos da lista
                         const listaDePalpitesString = listaDePalpite.join(", ");
-        
+
                         // Limpar o campo de entrada
                         document.getElementById('entrada').value = '';
-                        
+
                         const acertouElement = document.getElementById('acertou');
                         acertouElement.innerHTML = texto + "Números Digitados: " + listaDePalpitesString + "<br><br>" + "Você Acertou !!!";
 
@@ -75,52 +70,52 @@ function enviar(){
 
                         // Adicionei um som ao errar palpite
                         var audio = new Audio('aplausos.mp3');
-                        audio.addEventListener('canplaythrough', function() {
-                        audio.play();
+                        audio.addEventListener('canplaythrough', function () {
+                                audio.play();
                         });
-                        
+
                 }
-        } 
-        
-        if(palpite.value>num){
-                // Limpe os elementos de feedback de outros casos
-                document.getElementById('menor').innerHTML = '';
-                document.getElementById('acertou').innerHTML = '';
-            
-                const linha1 = "Palpite do Jogador: ";
-                const linha2 = "Número Sorteado: ";
-                const texto = linha1 + palpite.value + "<br>";
-                const digitado = palpite.value;
+        }
 
-                
-                // Adicione o palpite à lista.
-                listaDePalpite.push(palpite.value);
-            
-                // Use join() para criar uma string com os elementos da lista
-                const listaDePalpitesString = listaDePalpite.join(", ");
-            
-                // Limpar o campo de entrada
-                document.getElementById('entrada').value = '';
-                
-                const errouElement = document.getElementById('errou');
-                errouElement.innerHTML = texto + "Números Digitados: " + listaDePalpitesString + "<br><br>" + "Você errou !!!";
-                
-                // Adicione a classe CSS para tornar o texto vermelho
-                errouElement.classList.add('texto-vermelho');
-            
-                const maiorElement = document.getElementById('maior');
-                maiorElement.innerHTML = "Digite um palpite Menor que "+digitado+" e Maior que 60.";
-                
-                // Adicione a classe CSS para tornar o texto amarelo
-                maiorElement.classList.add('texto-amarelo');
+                if (palpite.value > num) {
+                        // Limpe os elementos de feedback de outros casos
+                        document.getElementById('menor').innerHTML = '';
+                        document.getElementById('acertou').innerHTML = '';
 
-                // Adicionei um som ao errar palpite
-                var audio = new Audio('faustao-errou.mp3');
-                audio.addEventListener('canplaythrough', function() {
-                audio.play();
-                });
-        }     
-       
+                        const linha1 = "Palpite do Jogador: ";
+                        const linha2 = "Número Sorteado: ";
+                        const texto = linha1 + palpite.value + "<br>";
+                        const digitado = palpite.value;
+
+
+                        // Adicione o palpite à lista.
+                        listaDePalpite.push(palpite.value);
+
+                        // Use join() para criar uma string com os elementos da lista
+                        const listaDePalpitesString = listaDePalpite.join(", ");
+
+                        // Limpar o campo de entrada
+                        document.getElementById('entrada').value = '';
+
+                        const errouElement = document.getElementById('errou');
+                        errouElement.innerHTML = texto + "Números Digitados: " + listaDePalpitesString + "<br><br>" + "Você errou !!!";
+
+                        // Adicione a classe CSS para tornar o texto vermelho
+                        errouElement.classList.add('texto-vermelho');
+
+                        const maiorElement = document.getElementById('maior');
+                        maiorElement.innerHTML = "Digite um palpite Menor que " + digitado + " e Maior que 60.";
+
+                        // Adicione a classe CSS para tornar o texto amarelo
+                        maiorElement.classList.add('texto-amarelo');
+
+                        // Adicionei um som ao acertar palpite
+                        var audio = new Audio('faustao-errou.mp3');
+                        audio.addEventListener('canplaythrough', function () {
+                                audio.play();
+                        });
+                }
+
 }
 
 
